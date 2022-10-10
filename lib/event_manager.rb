f22ce41a1f1fc4c6dd097206d81de204477652a3
swifteye1111 <hasak.josh@gmail.com>
2022-10-10 17:04:39 +0100

    Fix file locations.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,10 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_numbers(phone_number)
+  phone_number
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -38,14 +42,15 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('../form_letter.erb')
-erb_template = ERB.new template_letter
+#template_letter = File.read('../form_letter.erb')
+#erb_template = ERB.new template_letter
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
-  form_letter = erb_template.result(binding)
-  save_thank_you_letter(id, form_letter)
+  #legislators = legislators_by_zipcode(zipcode)
+  #form_letter = erb_template.result(binding)
+  #save_thank_you_letter(id, form_letter)
+  puts phone_number = clean_phone_numbers(row[:phone_number])
 end
