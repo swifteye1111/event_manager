f13d01dc8980b7f3bfaea9a1170b8c2e73472a37
swifteye1111 <hasak.josh@gmail.com>
2022-10-11 13:39:22 +0100

    Calculate optimal hour.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -10,12 +12,8 @@ def clean_phone_numbers(phone_number)
   phone_number = number_form(phone_number)
   if phone_number.length == 10
     phone_number
-  elsif phone_number.length == 11
-    if phone_number[0] == '1'
-      phone_number[1..10]
-    else
-      'Invalid phone number'
-    end
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..10]
   else
     'Invalid phone number'
   end
@@ -23,6 +21,16 @@ end
 
 def number_form(num)
   num.tr('^0-9', '')
+end
+
+def find_optimal_hour(hours)
+  hours.max_by { |i| hours.count(i) }
+end
+
+def find_optimal_window(hours)
+  opt_hour = find_optimal_hour(hours)
+  opt_end = opt_hour == 23 ? 0 : opt_hour + 1
+  "#{opt_hour}:00 - #{opt_end}:00"
 end
 
 def legislators_by_zipcode(zip)
@@ -57,8 +65,10 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('../form_letter.erb')
-erb_template = ERB.new template_letter
+#template_letter = File.read('../form_letter.erb')
+#erb_template = ERB.new template_letter
+
+hours = Array.new
 
 contents.each do |row|
   id = row[0]
@@ -67,5 +77,8 @@ contents.each do |row|
   # legislators = legislators_by_zipcode(zipcode)
   # form_letter = erb_template.result(binding)
   # save_thank_you_letter(id, form_letter)
-  puts clean_phone_numbers(row[:homephone])
+  #puts clean_phone_numbers(row[:homephone])
+  hours.push(Time.strptime(row[:regdate], '%m/%d/%Y %k:%M').hour)
 end
+
+puts "The optimal time is #{find_optimal_window(hours)}"
